d7131254e19b0bc3e48cae29b5c2708cc4d918fd
Rishabh Singh <rishabh.katheria@minddeft.net>
2021-06-22 14:43:52 +0530

    login functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,20 @@
-import { combineReducers } from 'redux';
-
-import { fetchDashboardBanners, fetchDashboard } from './auth.reducer';
-import { fetchNFTContractInstance, fetchNetworkId } from './web3.reducer';
-
+import { combineReducers } from "redux";
+import { fetchNonce, fetchAuth } from "./defi.reducer";
+import { fetchDashboardBanners, fetchDashboard } from "./auth.reducer";
+import {
+  fetchNFTContractInstance,
+  fetchNetworkId,
+  fetchWeb3Data,
+} from "./web3.reducer";
 
 const rootReducer = combineReducers({
-  fetchNFTContractInstance, fetchNetworkId, fetchDashboardBanners,
+  fetchAuth,
+  fetchNonce,
+  fetchNFTContractInstance,
+  fetchNetworkId,
+  fetchDashboardBanners,
   fetchDashboard,
+  fetchWeb3Data,
 });
 
 export default rootReducer;
