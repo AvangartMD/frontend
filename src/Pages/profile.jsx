172f7c56c30b12ab06fc16495e8e3379725d4ca9
Shivani Prajapati <shivani.prajapati@minddeft.com>
2021-07-01 19:13:24 +0530

    Reused the Artist and NFT card component

--- src/Pages/profile.jsx
+++ src/Pages/profile.jsx
@@ -3,17 +3,11 @@ import "react-tabs/style/react-tabs.css";
 import React, { Component } from "react";
 import styled from "styled-components";
 import Gs from "../Theme/globalStyles";
-import { Link } from "react-router-dom";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import dateFormat from "dateformat";
 
-import NFT2 from "../Assets/images/nft2.jpg";
-import UserImg from "../Assets/images/user-img.jpg";
-import HeartIcon from "../Assets/images/heart-icon.svg";
-import StarIcon from "../Assets/images/star-icon.svg";
-import RoundIcon from "../Assets/images/round-icon.svg";
 import AdBannerIMG from "../Assets/images/adbanner.jpg";
 import LoaderGif from "../Assets/images/loading.gif";
 import ProfielBack from "../Assets/images/profile-back.jpg";
@@ -31,6 +25,8 @@ import { actions } from "../actions";
 import { services } from "../services";
 import { compressImage } from "../helper/functions";
 import Drafts from "../Component/profile/drafts";
+import Artist from "../Component/profile/artits";
+
 
 class Profile extends Component {
   constructor(props) {
@@ -41,19 +37,11 @@ class Profile extends Component {
     this.state = {
       profile: { file: null, url: null },
       cover: { file: null, url: null },
-      tabPanel: "all",
-      loading: false,
     };
   }
 
   async componentDidMount() {
-    const { profile, categories, NFTs } = this.props;
-    if (!categories) {
-      this.props.getCategories(); // fetch categories
-    }
-    if (!NFTs) {
-      this.props.getNFTs(); // fetch user nft's
-    }
+    const { profile } = this.props;
     if (!profile) {
       this.props.getProfile(); // fetch profile
     }
@@ -98,62 +86,9 @@ class Profile extends Component {
     this.setState({ loading: false }); // stop loader
   };
 
-  renderTabPanel = (NFTs) => {
-    const { tabPanel } = this.state;
-    if (tabPanel !== "all") {
-      NFTs = NFTs.filter((nft) =>
-        nft.category.some((category) => category._id === tabPanel)
-      );
-    }
-    return NFTs.map((nft, key) => {
-      return (
-        <Gs.W25V2 key={key}>
-          <Gs.TenpxGutter>
-            <div className="NFT-home-box">
-              <NFTImgBX>
-                {" "}
-                <img src={nft.image.compressed} alt="" />{" "}
-              </NFTImgBX>
-              <div className="NFT-home-box-inner">
-                <h4>{nft.title}</h4>
-                <CollectionBar>
-                  <p>
-                    {nft.edition} <span>of 2500</span>
-                  </p>
-                  <p>
-                    <Link to="/">
-                      See the collection <i className="fas fa-angle-right"></i>
-                    </Link>
-                  </p>
-                </CollectionBar>
-                <Edition className="edition2">
-                  <div className="ed-box">
-                    <p>Current bid</p>
-                    <h3>{nft.price} BNB</h3>
-                  </div>
-                  <div className="ed-box">
-                    <p>Ending in</p>
-                    <h3>
-                      {nft.saleState === "AUCTION"
-                        ? `${nft.auctionTime}h`
-                        : "13h 12m 11s"}
-                    </h3>
-                  </div>
-                </Edition>
-                <UserImgName>
-                  <img src={UserImg} alt="" />@{nft.ownerId.username}
-                </UserImgName>
-              </div>
-            </div>
-          </Gs.TenpxGutter>
-        </Gs.W25V2>
-      );
-    });
-  };
-
   render() {
-    const { profile, categories, NFTs } = this.props;
-    const { tabPanel, loading } = this.state;
+    const { profile } = this.props;
+    const { loading } = this.state;
     return (
       <>
         <ProMBannerBX
@@ -399,50 +334,7 @@ class Profile extends Component {
               </TabList>
 
               <TabPanel>
-                <FilterMBX>
-                  <FilterLbx>
-                    <button
-                      className={tabPanel === "all" ? "active" : ""}
-                      id="all"
-                      onClick={() => {
-                        this.setState({ tabPanel: "all" });
-                      }}
-                    >
-                      All
-                    </button>
-                    {categories
-                      ? categories.map((category, key) => {
-                          return (
-                            <button
-                              id={category.id}
-                              key={key}
-                              className={
-                                tabPanel === category.id ? "active" : ""
-                              }
-                              onClick={() => {
-                                this.setState({ tabPanel: category.id });
-                              }}
-                            >
-                              {category.categoryName}
-                            </button>
-                          );
-                        })
-                      : "loading.."}
-                  </FilterLbx>
-                </FilterMBX>
-                <HomeNFTs>
-                  <Gs.Container>
-                    <NFTfourbox>
-                      {NFTs ? (
-                        this.renderTabPanel(NFTs)
-                      ) : (
-                        <LoaderBX>
-                          <img src={LoaderGif} alt="" />
-                        </LoaderBX>
-                      )}
-                    </NFTfourbox>
-                  </Gs.Container>
-                </HomeNFTs>
+                <Artist />
               </TabPanel>
               <TabPanel>2</TabPanel>
               <TabPanel>3</TabPanel>
@@ -699,298 +591,10 @@ const ADBannerMBX = styled(FlexDiv)`
     height: auto;
   }
 `;
+
 const LoaderBX = styled(FlexDiv)`
   width: 100%;
   margin: 50px auto;
-`;
-const HomeNFTs = styled.div`
-  width: 100%;
-  margin-top: 40px;
-  .home-title {
-    border-bottom: 1px solid #dddddd;
-    text-align: left;
-    margin-bottom: 30px;
-    h3 {
-      color: #000000;
-      font-size: 32px;
-      position: relative;
-      line-height: 32px;
-      margin: 0px 0px 15px;
-      padding-left: 20px;
-      letter-spacing: -1px;
-      :before {
-        content: "";
-        position: absolute;
-        left: 0px;
-        top: 12px;
-        width: 10px;
-        height: 10px;
-        background: url(${RoundIcon}) no-repeat;
-      }
-    }
-  }
-  .star-title {
-    text-align: left;
-    margin-bottom: 18px;
-    h3 {
-      color: #000000;
-      font-size: 32px;
-      position: relative;
-      line-height: 32px;
-      margin: 0px;
-      padding-left: 20px;
-      letter-spacing: -1px;
-      :before {
-        content: "";
-        position: absolute;
-        left: 0px;
-        top: 12px;
-        width: 12px;
-        height: 12px;
-        background: url(${StarIcon}) no-repeat;
-      }
-    }
-  }
-  .heart-title {
-    border-bottom: 1px solid #dddddd;
-    text-align: left;
-    margin-bottom: 30px;
-    h3 {
-      color: #000000;
-      font-size: 32px;
-      position: relative;
-      line-height: 32px;
-      margin: 0px 0px 15px;
-      padding-left: 20px;
-      letter-spacing: -1px;
-      :before {
-        content: "";
-        position: absolute;
-        left: 0px;
-        top: 12px;
-        width: 12px;
-        height: 11px;
-        background: url(${HeartIcon}) no-repeat;
-      }
-    }
-  }
-`;
-
-const NFTfirstbox = styled(FlexDiv)`
-  border: 1px solid #dddddd;
-  position: relative;
-  border-radius: 10px;
-  margin-bottom: 30px;
-  .w60 {
-    width: 60%;
-  }
-  .w40 {
-    width: 40%;
-  }
-`;
-const NFTfbleft = styled(FlexDiv)`
-  background-color: #eef2f7;
-  padding: 80px 0px;
-  img {
-    box-shadow: 20px 20px 40px 1px rgb(0 0 0 /30%);
-  }
-`;
-const NFTfbright = styled.div`
-  padding: 20px 50px;
-  h3 {
-    color: #000000;
-    font-size: 22px;
-    letter-spacing: -0.83px;
-    margin: 0px 0px 10px;
-    line-height: normal;
-  }
-  p {
-    color: #000000;
-    font-size: 12px;
-    letter-spacing: -0.53px;
-    margin: 0px 0px 10px;
-    line-height: 20px;
-  }
-  a {
-    color: #000000;
-    font-size: 10px;
-    font-weight: 600;
-    letter-spacing: -0.5px;
-    margin: 0px 0px 40px;
-    line-height: 20px;
-    display: block;
-    :hover {
-      color: #555;
-      text-decoration: underline;
-    }
-  }
-`;
-const NFTLike = styled(FlexDiv)`
-  width: 56px;
-  height: 34px;
-  position: absolute;
-  right: 20px;
-  top: 20px;
-  box-shadow: 0px 4px 5px 0px rgb(0 0 0 / 10%);
-  border-radius: 30px;
-  p {
-    color: #ff2a44;
-    font-size: 12px;
-    font-weight: 600;
-    margin: 0px;
-  }
-  a {
-    line-height: normal;
-    width: 15px;
-    height: 15px;
-    margin: 0px 4px 0px 0px;
-  }
-`;
-
-const Edition = styled(FlexDiv)`
-  justify-content: space-between;
-  background-color: #eef2f7;
-  border-radius: 10px;
-  padding: 16px 20px;
-  margin: 0px 0px 40px;
-  .ed-box {
-    p {
-      color: #8e9194;
-      font-size: 12px;
-      letter-spacing: -0.6px;
-      font-weight: 600;
-      margin: 0px 0px 5px;
-    }
-    h3 {
-      color: #000;
-      font-size: 20px;
-      letter-spacing: -0.89px;
-      font-weight: 700;
-      margin: 0px;
-      span {
-        font-size: 10px;
-        font-weight: 300;
-        letter-spacing: -0.44px;
-      }
-    }
-  }
-`;
-
-const UserImgName = styled(FlexDiv)`
-  justify-content: flex-start;
-  color: #000;
-  font-size: 14px;
-  letter-spacing: -0.7px;
-  font-weight: 600;
-  margin: 0px;
-  img {
-    border-radius: 50%;
-    margin-right: 10px;
-    width: 32px;
-    height: 32px;
-  }
-`;
-
-const NFTfourbox = styled(FlexDiv)`  
-    flex-wrap:wrap; margin:0px -10px 50px; 
-    .row{margin:0px -10px;}
-    img.main{width:100%; border-top-left-radius:10px; border-top-right-radius:10px;}
-        .NFT-home-box{ border-radius:10px; border:1px solid #dddddd; 
-          :hover{ box-shadow: 0 10px 10px 0 rgba(0, 0, 0, 0.2);} 
-          .NFT-home-box-inner{ padding:20px 15px;
-            h4{margin:0px 0px 10px; font-size:18px; color:#000000; font-weight:600; line-height:normal; letter-spacing:-0.67px;}
-            .edition2
-            {
-              justify-content:flex-start; padding:10px 15px; margin-bottom:20px;
-              .ed-box{
-                margin-right:20px;
-                p{font-size:10px; letter-spacing:-0.5px; margin:0px;}
-                h3{font-size:16px; letter-spacing:-0.71px;}
-                button{font-size:10px; color:#000; letter-spacing:-0.36px; font-weight:600; line-height:normal; padding:10px 17px; border-radius:13px; border:1px solid #000000;
-                  :hover{background-color:#d121d6; color:#fff; border-color:#d121d6;}
-                }
-              }
-            }
-            .JCSB{justify-content:space-between;
-              .ed-box{margin-right:0px;}
-            }
-          }
-        }
-      }
-      .JCSB {
-        justify-content: space-between;
-        .ed-box {
-          margin-right: 0px;
-        }
-      }
-    }
-  }
-`;
-
-const NFTImgBX = styled(FlexDiv)`
-  width: 100%;
-  height: 253px;
-  border-radius: 10px 10px 0 0;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-`;
-
-const CollectionBar = styled(FlexDiv)`
-  justify-content: space-between;
-  margin-bottom: 20px;
-  p {
-    font-size: 14px;
-    letter-spacing: -0.62px;
-    font-weight: 600;
-    margin: 0px;
-    color: #000;
-    span {
-      font-size: 12px;
-      letter-spacing: -0.53px;
-      font-weight: 300;
-    }
-    a {
-      font-size: 10px;
-      letter-spacing: -0.5px;
-      font-weight: 600;
-      color: #000;
-      :hover {
-        color: #555;
-        text-decoration: underline;
-      }
-    }
-  }
-`;
-
-const ViewallButton = styled.div`
-  text-align: center;
-  margin-bottom: 120px;
-  button {
-    border: 1px solid #000000;
-    border-radius: 10px;
-    width: 190px;
-    height: 44px;
-    margin: 0 auto;
-    font-size: 14px;
-    font-weight: 600;
-    letter-spacing: -0.5px;
-    :hover {
-      background-color: #000;
-      color: #fff;
-    }
-  }
-`;
-
-const CollectionSection = styled(FlexDiv)`
-  margin: 0px -10px 50px;
-  img {
-    border-radius: 10px;
-    border: 1px solid #dddddd;
-  }
 `;
 
 const HomeTabs = styled.div`
@@ -1128,144 +732,6 @@ const AdBanner = styled.div`
   }
 `;
 
-const AdBanner2 = styled.div`
-  padding: 120px 0px;
-  margin: 120px 0px 0px;
-  text-align: center;
-  background: url(${AdBannerIMG}) no-repeat;
-  background-size: cover;
-  h2 {
-    color: #000000;
-    font-size: 42px;
-    letter-spacing: -2px;
-    font-weight: bold;
-    line-height: normal;
-    max-width: 680px;
-    width: 100%;
-    margin: 0 auto 30px;
-  }
-  p {
-    color: #000000;
-    font-size: 20px;
-    letter-spacing: -0.5px;
-    font-weight: 700;
-    line-height: normal;
-    max-width: 680px;
-    width: 100%;
-    margin: 0 auto 50px;
-    :last-child {
-      margin-bottom: 0px;
-    }
-  }
-  button {
-    background-color: #f40058;
-    color: #fff;
-    font-size: 14px;
-    letter-spacing: -0.5px;
-    font-weight: 700;
-    border-radius: 15px;
-    width: 190px;
-    height: 44px;
-    :hover {
-      background-color: #000;
-    }
-  }
-`;
-const FilterMBX = styled(FlexDiv)`
-  width: 100%;
-  justify-content: space-between;
-  max-width: 1080px;
-  margin: 30px auto 0 auto;
-`;
-
-const FilterLbx = styled(FlexDiv)`
-  width: 45%;
-  justify-content: flex-start;
-
-  button {
-    display: inline-block;
-    padding: 10px 25px;
-    font-size: 14px;
-    font-weight: 600;
-    color: #000000;
-    border-radius: 15px;
-    background-color: #eef2f7;
-    margin-right: 8px;
-
-    &.active {
-      background-color: #00babc;
-      color: #fff;
-    }
-    :hover {
-      background-color: #00babc;
-      color: #fff;
-      box-shadow: 0 10px 10px 0 rgba(0, 0, 0, 0.2);
-    }
-  }
-`;
-const FilterRbx = styled(FlexDiv)`
-  width: 55%;
-  justify-content: flex-end;
-`;
-const FilterInputBX = styled(FlexDiv)`
-  width: 100%;
-  max-width: 220px;
-  position: relative;
-  margin-right: 9px;
-
-  input {
-    background-color: #eef2f7;
-    font-size: 14px;
-    border-radius: 15px;
-    border: 1px solid transparent;
-    outline: none;
-    height: 38px;
-    width: 100%;
-    padding: 3px 3px 3px 40px;
-    :focus {
-      background-color: #fff;
-      border: 1px solid #00babc;
-      box-shadow: 0 10px 10px 0 rgba(0, 0, 0, 0.2);
-    }
-  }
-`;
-const SearchICO = styled(FlexDiv)`
-  width: 21px;
-  height: 21px;
-  position: absolute;
-  left: 11px;
-  top: 9px;
-`;
-const FilterICO = styled(FlexDiv)`
-  width: 21px;
-  height: 21px;
-  position: absolute;
-  left: 11px;
-  top: 9px;
-`;
-const FilterBAR = styled(FlexDiv)`
-  width: 100%;
-  max-width: 220px;
-  justify-content: flex-start;
-  position: relative;
-  background-color: #eef2f7;
-  border-radius: 15px;
-  border: 0px;
-  outline: none;
-  height: 38px;
-  padding: 3px 3px 3px 40px;
-  font-size: 14px;
-  color: #000000;
-  cursor: pointer;
-  border: 1px solid transparent;
-  &.active,
-  &:hover {
-    background-color: #fff;
-    border: 1px solid #00babc;
-    box-shadow: 0 10px 10px 0 rgba(0, 0, 0, 0.2);
-  }
-`;
-
 const OnbTitle01 = styled.div`
   font-size: 26px;
   font-weight: 600;
@@ -1304,33 +770,17 @@ const WhiteBX01 = styled(FlexDiv)`
   align-content: center;
 `;
 
-const CloseBTN = styled.button`
-  width: 20px;
-  height: 20px;
-  position: absolute;
-  right: 20px;
-  top: 27px;
-  padding: 0;
-  margin: 0px;
-  :hover {
-    transform: rotate(90deg);
-  }
-`;
 
 const mapDipatchToProps = (dispatch) => {
   return {
     getProfile: () => dispatch(actions.getUserDetails()),
     updateProfile: (params) => dispatch(actions.updateUserDetails(params)),
-    getCategories: () => dispatch(actions.fetchCategories()),
-    getNFTs: () => dispatch(actions.getUserNFT()),
   };
 };
 const mapStateToProps = (state) => {
   return {
     profile: state.fetchAuthData,
     updated: state.updateProfile,
-    categories: state.fetchCategory,
-    NFTs: state.fetchUserNFT,
   };
 };
 
