1caab3a26673181fa00868c9cc275a79b5b15d3e
RishabhMinddeft <rishabh.katheria@minddeft.net>
2021-09-16 18:25:37 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
-import Wrapper from './Component/wrapper';
+require("file-loader?name=[name].[ext]!./index.html");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store";
+import Wrapper from "./Component/wrapper";
 
 ReactDOM.render(
   <Wrapper>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
   </Wrapper>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
