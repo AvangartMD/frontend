7afcaa3de622605c322c02cb15d4def0077025c8
Shivani Prajapati <shivani.prajapati@minddeft.com>
2021-10-04 19:35:57 +0530

    Reduced the cookie time to 1 hour

--- src/config/index.js
+++ src/config/index.js
@@ -13,8 +13,8 @@ export const awsIdentityPoolId =
   'eu-central-1:949b9487-ed35-4eb3-a2d3-1866e6ad890b';
 export const awsBucket = 'avangrat-development';
 
-// set expiry time for the localstorage data
-export const expiryTime = 7200000; // 2 hours into miliseconds
+// set expiry time for the localstorage/cookie data
+export const expiryTime = 2; // 2 hours
 
 
 // Instragram config 
